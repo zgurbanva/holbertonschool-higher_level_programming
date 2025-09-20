3cdcb72510b48c9e5dcc3b2dcd30816a3b2d84f6
Zeynab <zeyneb.gurbanva@gmail.com>
2025-09-20 11:05:57 +0000

    commit

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square.
+The purpose is to serve as the foundation for future
+Square-related features.
+"""
+
+
+class Square:
+    """
+    This is an empty class that defines a square.
+    Currently, it does not have any attributes or methods.
+    """
+    pass
+
