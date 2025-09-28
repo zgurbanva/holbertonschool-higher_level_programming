5dbac9245eacab0f98bb25d06e3c15c84ff913a7
Zeynab <zeyneb.gurbanva@gmail.com>
2025-09-28 13:19:30 +0000

    commit

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class BaseGeometry to be extended later.
+    """
+    pass
