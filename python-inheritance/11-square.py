b95a052e958e37031b88bbb59638d787a57bf109
Zeynab <zeyneb.gurbanva@gmail.com>
2025-09-28 15:43:42 +0000

    commit

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""
-This module defines the Square class with __str__ override.
-"""
+"""Defines class Square that inherits from Rectangle (9-rectangle.py)."""
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    Square defines a square with equal sides and custom string output.
-    """
+    """Square class, inherits from Rectangle"""
 
     def __init__(self, size):
-        """
-        Initialize a Square with validated size.
-
-        Args:
-            size (int): The size of the square.
-        """
+        """Initialize a square with size (validated)."""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """
-        Return the area of the square.
-        """
-        return self.__size * self.__size
+        """Return the area of the square."""
+        return self.__size ** 2
 
     def __str__(self):
-        """
-        Return the string representation of the square.
-        """
-        return f"[Square] {self.__size}/{self.__size}"
+        """Return the string representation of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
