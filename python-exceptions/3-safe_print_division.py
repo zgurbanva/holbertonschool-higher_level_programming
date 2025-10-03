f492039d9bc931fba80e3274872544128cdd1d16
Zeynab <zeynab.gurbanva@gmail.com>
2025-10-03 04:42:26 +0000

    commit

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Divides 2 integers and prints the result"""
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
