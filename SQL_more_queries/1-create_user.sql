872c6584d12549611cf427f624eb5a0c88fd6bf6
Zeynab <zeyneb.gurbanva@gmail.com>
2025-10-26 18:54:05 +0000

    commit

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- CREATE user_0d_1 WITH FULL PRIVILEGES
--- Creates 'user_0d_1'@'localhost' with password and grants all privileges server-wide
+-- A script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
