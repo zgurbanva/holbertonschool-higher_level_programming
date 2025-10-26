698516897e5d1aaeb9bb4a0462d3c7af19f3a90c
Zeynab <zeyneb.gurbanva@gmail.com>
2025-10-26 16:27:34 +0000

    commit

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- LIST PRIVILEGES FOR user_0d_1 AND user_0d_2 ON localhost
+-- Prints all grants for user_0d_1@localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Prints all grants for user_0d_2@localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
