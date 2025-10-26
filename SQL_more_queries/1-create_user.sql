679fb27dc8b156772ec6158d10c3d481f94659c8
Zeynab <zeyneb.gurbanva@gmail.com>
2025-10-26 16:49:13 +0000

    commit

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
 -- CREATE user_0d_1 WITH FULL PRIVILEGES
--- Creates user 'user_0d_1'@'localhost' with password and grants all privileges
+-- Creates 'user_0d_1'@'localhost' with password and grants all privileges server-wide
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant full privileges on the server (all databases/tables)
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
