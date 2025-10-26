2f6c433d829abd04543e589d1d8e674023a90dc5
Zeynab <zeyneb.gurbanva@gmail.com>
2025-10-26 17:50:01 +0000

    commit

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa using MySQLdb.
-Usage: ./0-select_states.py <mysql_user> <mysql_password> <database_name>
-Results are sorted by states.id in ascending order and printed as tuples.
-"""
-
+"""List all states from the given MySQL database ordered by states.id."""
 import sys
 import MySQLdb
 
 
 def main():
-    """Connects to MySQL and prints all rows from states ordered by id ASC."""
-    user, passwd, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=user,
-        passwd=passwd,
-        db=db_name,
-        charset="utf8"
-    )
+    """Connect to MySQL on localhost:3306 and print rows from the states table ordered by id."""
+    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=user, passwd=passwd, db=db)
     cur = conn.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
-    rows = cur.fetchall()
-    for row in rows:
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    for row in cur.fetchall():
         print(row)
-
     cur.close()
     conn.close()
 
