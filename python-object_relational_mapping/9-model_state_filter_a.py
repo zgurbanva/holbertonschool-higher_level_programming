557a7dead3c230984cf2b5f3f5aca7ef8d757568
Zeynab <zeyneb.gurbanva@gmail.com>
2025-10-26 18:45:01 +0000

    commit

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-"""
-List all State objects that contain the letter 'a' from the database.
-
-Usage:
-    ./9-model_state_filter_a.py <mysql_username> <mysql_password> <db_name>
-
-The output is sorted by states.id ascending and printed as:
-<id>: <name>
-"""
+"""List all State objects that contain the letter 'a' (sorted by id asc)."""
 
 import sys
 from sqlalchemy import create_engine
@@ -15,27 +7,17 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-def main():
-    """Entrypoint for filtering states containing 'a'."""
-    user, pwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
-    uri = f"mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}"
-    engine = create_engine(uri, pool_pre_ping=True)
+if __name__ == "__main__":
+    if len(sys.argv) != 3 + 1:
+        sys.exit(1)
 
+    user, pwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine(f"mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}",
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    try:
-        q = (session.query(State)
-             .filter(State.name.ilike('%a%'))
-             .order_by(State.id.asc()))
+    for s in session.query(State).filter(State.name.like('%a%')).order_by(State.id):
+        print(f"{s.id}: {s.name}")
 
-        for s in q:
-            print(f"{s.id}: {s.name}")
-    finally:
-        session.close()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        sys.exit(1)
-    main()
+    session.close()
